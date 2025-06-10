b0fed04883a8b1caf46b87aa01ed99930f46732c
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-06-10 14:57:44 -0300

    Ejecute el comando dotnet new console y realice los primeros controles que pide el punto 1

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+
+string pathB;
+
+do
+{
+    Console.WriteLine("Ingresar el PATH del directorio que desea analizar:");
+    pathB = Console.ReadLine();
+
+    if (Directory.Exists(pathB))
+    {
+        Console.WriteLine("El directorio " + pathB + " existe");
+    }
+    else
+    {
+        Console.WriteLine("El directorio " + pathB + " no existe");
+    }
+
+    Console.WriteLine("Ingresar un PATH valido: ");
+    pathB = Console.ReadLine();
+
+} while (!Directory.Exists(pathB));
+
+Console.WriteLine("-----CARPETAS-----");
+
+String[] carpetas = Directory.GetDirectories(pathB);
+
+foreach (var carpeta in carpetas)
+{
+    Console.WriteLine(Path.GetFileName(carpeta));
+}
